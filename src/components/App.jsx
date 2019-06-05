9fff6018e4ad67fb25b89f9089c0a9f227e967a1
Sviatlana Kruhlikava <44160392+SviatlanKa@users.noreply.github.com>
2019-06-05 20:26:45 +0300

    build structure of project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import Dispay from './Display';
 import DrampPad from './DramPad';
+import dataStorage from '../data/dataStorage';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: '',
+            href: '',
+            name: ''
+        }
+        this.getData = this.getData.bind(this);
+    }
+
+    const
+
+    getData(dataStorage) {
+        dataStorage.forEach()
+    }
+
     render() {
         return (
             <div id="drum-machine">Drum Machine
